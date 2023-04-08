7dbb48520e9433e94e5fce6f34c6c579c819d3fd
flowtyf <taiao.kawiti@developersinstitute.co.nz>
2023-04-08 12:52:04 +1200

    Update calculator function to allow multiple numbers

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ let firstNum = 0;
 let secondNum = 0;
 let operator = '';
 let result = 0;
-let calculated = false;
+let firstOp = false;
 
 //Display current number and operator
 //TODO: allow 1 number to equal whatever was input instead of undefined
@@ -34,36 +34,42 @@ function updateDisplay(btn) {
   }
 
   if (btn <= 9) {
-    if (!calculated) {
-      calcDisplay.innerText += btn;
-    }
-    if (calculated) {
-      clearDisplay();
-      calculated = false;
-      calcDisplay.innerText += btn;
-    }
+    calcDisplay.innerText += btn;
   }
 
   if (operators.includes(btn)) {
-    firstNum = parseFloat(calcDisplay.innerText);
-    operator = btn;
-    opDisplay.innerText = btn;
-    calcDisplayTwo.innerText = parseFloat(calcDisplay.innerText);
-    calcDisplay.innerText = '';
+    if (firstOp === false) {
+      firstOp = true;
+      firstNum = parseFloat(calcDisplay.innerText);
+      operator = btn;
+      opDisplay.innerText = btn;
+      calcDisplayTwo.innerText = parseFloat(calcDisplay.innerText);
+      calcDisplay.innerText = '';
+    } else if (firstOp === true) {
+      secondNum = parseFloat(calcDisplay.innerText);
+      result = operate(firstNum, secondNum, operator);
+      operator = btn;
+      firstNum = result;
+      opDisplay.innerText = btn;
+      calcDisplayTwo.innerText = result;
+      calcDisplay.innerText = '';
+    }
   }
 
   if (btn === '=') {
     secondNum = parseFloat(calcDisplay.innerText);
     clearDisplay();
     result = operate(firstNum, secondNum, operator);
-    calcDisplay.innerText = result;
-    firstNum = result;
-    secondNum = 0;
+    firstOp = false;
+    if (result === undefined) {
+      calcDisplay.innerText =
+        'What if I told you that, despite what you may have learned in school, you can divide by zero if you just think of it in the right way? And what if the answer you get not only had real-world significance, but could actually explain why other parts of math work the way they do? If you’re not afraid to question what you’ve been told, and you’re willing to be flexible with math, then read onward to discover…';
+    } else calcDisplay.innerText = result;
   }
 
   if (btn === 'C') {
     clearDisplay();
-    calculated = false;
+    firstOp = false;
     firstNum = 0;
     secondNum = 0;
     operator = '';
