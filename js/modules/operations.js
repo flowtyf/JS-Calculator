a2b0e275e6985d2f3051619f7d90cc4fc5f59f04
flowtyf <taiao.kawiti@developersinstitute.co.nz>
2023-04-08 13:09:56 +1200

    Update CSS

--- js/modules/operations.js
+++ js/modules/operations.js
@@ -15,9 +15,6 @@ function multiply(a, b) {
 
 function divide(a, b) {
   const divideTotal = a / b;
-  if (b === 0) {
-    return 'What if I told you that, despite what you may have learned in school, you can divide by zero if you just think of it in the right way? And what if the answer you get not only had real-world significance, but could actually explain why other parts of math work the way they do?';
-  }
   return divideTotal;
 }
 
